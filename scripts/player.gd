40ea385725a07bb06e007f3f9bd4b0f5925b712f
Fatih <45256657+fatihceyln@users.noreply.github.com>
2026-03-29 16:02:24 +0300

    use custom input actions

--- scripts/player.gd
+++ scripts/player.gd
@@ -11,15 +11,16 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
+		# Klavye bırakıldığında yatay hızı sıfıra düşürür. Her seferde SPEED değeri kadar düşürür.
+		# move_toward(100, 0, 30) → 70 (30 azalır)
+		# move_toward(20, 0, 30) → 0 (20’yi aşıp negatife gitmez)
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
