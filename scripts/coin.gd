1ac9ef6ef8b920f240a9dd3c5ae35375d5375ec6
Fatih <45256657+fatihceyln@users.noreply.github.com>
2026-03-29 17:56:33 +0300

    Add coin pickup animation

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,7 +1,12 @@
 extends Area2D
 
 @onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_body_entered(body: Node2D) -> void:
 	game_manager.add_point()
-	queue_free()
+	animation_player.play("pickup")
+
+
+func _on_tree_exited() -> void:
+	print("tree exited")
