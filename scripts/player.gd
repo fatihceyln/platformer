336aba687083ba0096343e068f4c862639346a9e
Fatih <45256657+fatihceyln@users.noreply.github.com>
2026-03-29 17:02:57 +0300

    Add animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,17 +4,40 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	var direction := Input.get_axis("move_left", "move_right")
+	add_gravity(delta)
+	handle_jump()
+	flip_sprite(direction)
+	play_animations(direction)
+	move(direction)
+
+func add_gravity(delta: float):
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+func handle_jump():
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	var direction := Input.get_axis("move_left", "move_right")
+func flip_sprite(direction: float):
+	if direction > 0:
+			animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+
+func play_animations(direction: float):
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+
+func move(direction: float):
 	if direction:
 		velocity.x = direction * SPEED
 	else:
