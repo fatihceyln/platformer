aedf65ffe0a1c7371f2263bbd7712c7af3ee099b
Fatih <45256657+fatihceyln@users.noreply.github.com>
2026-03-29 15:47:18 +0300

    Update killzone.gd

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,7 +3,10 @@ extends Area2D
 @onready var timer: Timer = $Timer
 
 func _on_body_entered(body: Node2D) -> void:
+	body.get_node("CollisionShape2D").queue_free() # remove collision shape node from the player. body is player.
+	Engine.time_scale = 0.5
 	timer.start()
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
