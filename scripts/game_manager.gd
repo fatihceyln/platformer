189ea5fe9222bc1aa06e0992901a0015ba0e9948
Fatih <45256657+fatihceyln@users.noreply.github.com>
2026-03-30 21:10:22 +0300

    Add screen transition using shader

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,14 +2,11 @@ class_name GameManager extends Node
 
 var score: int = 0
 @onready var score_label: Label = $ScoreLabel
-@onready var game_over_dim: ColorRect = $Overlay/Dim
-@onready var game_over_label: Label = $Overlay/GameOverLabel
-
+@onready var game_over_reveal: AnimationPlayer = $GameOverReveal
 
 func show_game_over_overlay() -> void:
-	game_over_dim.visible = true
-	game_over_label.visible = true
-
+	game_over_reveal.speed_scale = 1.0 / Engine.time_scale
+	game_over_reveal.play("game_over_reveal")
 
 func add_point() -> void:
 	score += 1
