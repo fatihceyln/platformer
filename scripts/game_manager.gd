a298f11ba14f2bb5a33a350f9570bcb37bd563ef
Fatih <45256657+fatihceyln@users.noreply.github.com>
2026-03-30 18:22:36 +0300

    Add game over overlay

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,6 +2,14 @@ class_name GameManager extends Node
 
 var score: int = 0
 @onready var score_label: Label = $ScoreLabel
+@onready var game_over_dim: ColorRect = $Overlay/Dim
+@onready var game_over_label: Label = $Overlay/GameOverLabel
+
+
+func show_game_over_overlay() -> void:
+	game_over_dim.visible = true
+	game_over_label.visible = true
+
 
 func add_point() -> void:
 	score += 1
--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -5,6 +5,9 @@ class_name Killzone extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	body.get_node("CollisionShape2D").queue_free() # remove collision shape node from the player. body is player.
 	Engine.time_scale = 0.5
+	var game_manager: GameManager = get_parent().get_node("%GameManager") as GameManager
+	if game_manager:
+		game_manager.show_game_over_overlay()
 	timer.start()
 
 func _on_timer_timeout() -> void:
