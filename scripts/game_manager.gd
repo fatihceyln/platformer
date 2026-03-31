fbd5e434da3a47ddab5d5189d61fa1a1211ec1c2
Fatih <45256657+fatihceyln@users.noreply.github.com>
2026-03-31 12:17:13 +0300

    Decouple `Timer` and `GameManager` from `Killzone`. `Killzone` now only emits `player_died` on the global `Events` autoload and removes the player's hitbox (unique in the scene). `GameManager` listens for that signal and orchestrates the death flow.

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -4,10 +4,33 @@ var score: int = 0
 @onready var score_label: Label = $ScoreLabel
 @onready var game_over_reveal: AnimationPlayer = $GameOverReveal
 
-func show_game_over_overlay() -> void:
-	game_over_reveal.speed_scale = 1.0 / Engine.time_scale
-	game_over_reveal.play("game_over_reveal")
+func _ready() -> void:
+	Events.player_died.connect(_on_player_died)
 
 func add_point() -> void:
 	score += 1
 	score_label.text = "You collected %d coins." %score
+	
+func _on_player_died() -> void:
+	print("on player died called")
+	Engine.time_scale = 0.5
+	_start_timer()
+	game_over_reveal.speed_scale = 1.0 / Engine.time_scale
+	game_over_reveal.play("game_over_reveal")
+
+func _start_timer() -> void:
+	var timer: Timer = Timer.new()
+	timer.wait_time = 0.7
+	timer.ignore_time_scale = true
+	timer.one_shot = true
+	timer.autostart = true
+	timer.timeout.connect(_timer_timed_out.bind(timer))
+	add_child(timer)
+
+func _timer_timed_out(timer: Timer) -> void: 
+	# We do not need to call `queue_free()` on timer to remove from the hierarchy.
+	# Because we are reloading the whole scene.
+	# But I will leave it for demoing purposes.
+	timer.queue_free() 
+	Engine.time_scale = 1.0
+	get_tree().reload_current_scene()
--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,15 +1,6 @@
 class_name Killzone extends Area2D
 
-@onready var timer: Timer = $Timer
-
 func _on_body_entered(body: Node2D) -> void:
-	body.get_node("CollisionShape2D").queue_free() # remove collision shape node from the player. body is player.
-	Engine.time_scale = 0.5
-	var game_manager: GameManager = get_parent().get_node("%GameManager") as GameManager
-	if game_manager:
-		game_manager.show_game_over_overlay()
-	timer.start()
-
-func _on_timer_timeout() -> void:
-	Engine.time_scale = 1.0
-	get_tree().reload_current_scene()
+	# Remove the collision shape node from the player. Body is player.
+	body.get_node_or_null("%PlayerHitBox").queue_free()
+	Events.player_died.emit()
